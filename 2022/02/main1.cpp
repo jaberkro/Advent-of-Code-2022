f6c1f236a637575b6a6a82d3a5a9a4d6f8f74b17
Jaberkro <jaberkro@student.codam.nl>
2022-12-03 17:41:02 +0100

    part 1 and 2 working, divided in main1 and main2

---
+++ 2022/02/main1.cpp
@@ -0,0 +1,61 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   main1.cpp                                          :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: jaberkro <jaberkro@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2022/12/01 06:20:17 by jaberkro      #+#    #+#                 */
+/*   Updated: 2022/12/03 17:37:35 by jaberkro      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <algorithm>
+
+int main(void)
+{
+	int	result = 0;
+	std::string line;
+	std::ifstream myfile("input.txt");
+
+	if (myfile.is_open())
+	{
+		while ( getline (myfile,line) )
+		{
+			result += line.at(2) - 87;
+			if ((line.at(2) - line.at(0)) % 3 == 0)
+				result += 6;
+			else if (line.at(2) - line.at(0) == 23)
+				result += 3;
+		}
+		std::cout << "result: " << result << std::endl;
+		myfile.close();
+	}
+  	else std::cout << "Unable to open file"; 
+  	return (0);
+}
+
+/*
+Explanation
+
+By substracting 87 of the ascii value of my input I get the score of the shape I selected.
+
+I calculated the distance between the ascii values of the possible inputs. 
+By checking the outcomes of the matches I concluded that:
+- a difference of 21 or 24 implies a win scenario (a.k.a. any difference of which % 3 == 0)
+- a difference of 23 always implies a draw scenario
+- a difference of 22 or 25 implies a lose scenario
+
+a x		23 draw
+a y		24 win
+a z		25 lose
+b x		22 lose
+b y		23 draw
+b z		24 win
+c x		21 win
+c y 	22 lose
+c z		23 draw
+*/
---
+++ 2022/02/main2.cpp
@@ -0,0 +1,80 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   main2.cpp                                          :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: jaberkro <jaberkro@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2022/12/03 17:01:03 by jaberkro      #+#    #+#                 */
+/*   Updated: 2022/12/03 17:37:30 by jaberkro      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <algorithm>
+
+int	calculate_round_score(std::string line)
+{
+	int	score;
+
+	score = (line.at(2) - 88) * 3;
+	if (line.at(2) == 'X')
+	{
+		if (line.at(0) == 'A')
+			score += (line.at(0) - 64 + 2);
+		else
+			score += (line.at(0) - 64 - 1);
+	}
+	else if (line.at(2) == 'Y')
+		score += line.at(0) - 64;
+	else
+	{
+		if (line.at(0) == 'C')
+			score += (line.at(0) - 64 - 2);
+		else
+			score += (line.at(0) - 64 + 1);
+	}
+	return (score);
+}
+
+int main(void)
+{
+	int	result = 0;
+	std::string line;
+	std::ifstream myfile("input.txt");
+
+	if (myfile.is_open())
+	{
+		while ( getline (myfile,line) )
+		{
+			result += calculate_round_score(line);
+		}
+		std::cout << "result: " << result << std::endl;
+		myfile.close();
+	}
+  	else std::cout << "Unable to open file"; 
+  	return (0);
+}
+
+/*
+Explanation
+
+By substracting 88 of the x/y/z value and multiplying that by 3 I get the score of the outcome of the round.
+
+In a lose scenario I should play (opponents move - 1) (except in case a x, then I should play opponents move + 2)
+In a draw scenario I should play opponents move
+In a win scenario I should play (opponents move + 1) (except in case c z, then I should play opponents move - 2)
+
+input	my move
+a x		c lose
+a y		a draw
+a z		b win
+b x		a lose
+b y		b draw
+b z		c win
+c x		b lose
+c y 	c draw
+c z		a win
+*/
