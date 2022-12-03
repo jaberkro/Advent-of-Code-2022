ddde83adde912e0308ceda938429dcee95c9e5c0
Jaberkro <jaberkro@student.codam.nl>
2022-12-03 16:29:58 +0100

    std=c++98

--- 2022/01/main.cpp
+++ 2022/01/main.cpp
@@ -6,39 +6,19 @@
 /*   By: jaberkro <jaberkro@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/12/01 06:20:17 by jaberkro      #+#    #+#                 */
-/*   Updated: 2022/12/01 06:57:37 by jaberkro      ########   odam.nl         */
+/*   Updated: 2022/12/03 16:25:39 by jaberkro      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include <fstream>
-
-void update_max(int result, int *max1, int *max2, int *max3)
-{
-	if (result > *max3)
-	{
-		if (result > *max2)
-		{
-			*max3 = *max2;
-			if (result > *max1)
-			{
-				*max2 = *max1;
-				*max1 = result;
-			}
-			else
-				*max2 = result;
-		}
-		else
-			*max3 = result;	
-	}
-}
+#include <vector>
+#include <algorithm>
 
 int main(void)
 {
 	int	result = 0;
-	int max1 = 0;
-	int max2 = 0;
-	int max3 = 0;
+	std::vector<int> calories;
 	std::string line;
 	std::ifstream myfile("input.txt");
 	if (myfile.is_open())
@@ -47,14 +27,16 @@ int main(void)
 		{
 			if (line.compare("") == 0)
 			{
-				update_max(result, &max1, &max2, &max3);
+				calories.push_back(result);
 				result = 0;
 			}
 			else
 				result += stoi(line);
 		}
-		std::cout << "max calories top 3: " << max1 << ", " << max2 << ", " << max3 << std::endl;
-		std::cout << "sum of 3 max calories: " << max1 + max2 + max3 << std::endl;
+		std::sort(calories.begin(), calories.end());
+		std::reverse(calories.begin(), calories.end());
+		std::cout << "max calories top 3: " << calories.at(0) << ", " << calories.at(1) << ", " << calories.at(2) << std::endl;
+		std::cout << "sum of 3 max calories: " << calories.at(0) + calories.at(1) + calories.at(2) << std::endl;
 		myfile.close();
 	}
   else std::cout << "Unable to open file"; 
